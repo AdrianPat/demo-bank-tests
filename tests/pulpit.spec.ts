a80b7590f8db091ebabe2578f7e66a67c639b53f
AdrianPat <adrianpatycki@gmail.com>
2024-10-07 18:18:03 +0200

    run scripts added

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -46,4 +46,22 @@ test.describe('Pulpit tests', () => {
         await expect(page.locator('#show_messages')).toHaveText(
             `Doładowanie wykonane! ${topUpAmount},00PLN na numer ${receiverPhoneNumber}`);
     });
+
+    test('Correct balance after successful phone top-up', async ({ page }) => {
+        // Arrange
+        const receiverPhoneNumber = '500 xxx xxx';
+        const topUpAmount = '30';
+        const initialBalance = await page.locator('#money_value').innerText();
+        const expectedBalance = Number(initialBalance) - Number(topUpAmount);
+
+        // Act
+        await page.locator('#widget_1_topup_receiver').selectOption(receiverPhoneNumber);
+        await page.locator('#widget_1_topup_amount').fill(topUpAmount);
+        await page.locator('#uniform-widget_1_topup_agreement span').click();
+        await page.getByRole('button', { name: 'doładuj telefon' }).click();
+        await page.getByTestId('close-button').click();
+
+        // Assert
+        await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
+    });
 });
