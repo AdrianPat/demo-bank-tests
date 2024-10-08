0d232b8ddc6f6ff555003aba32c23fd761411c2a
AdrianPat <adrianpatycki@gmail.com>
2024-10-08 18:53:01 +0200

    added simple POM

--- tests/payment.spec.ts
+++ tests/payment.spec.ts
@@ -1,5 +1,6 @@
 import test, { expect } from "@playwright/test";
 import { loginData } from "../test-data/login.data";
+import { LoginPage } from "../pages/login.page";
 
 test.describe('Payment in Demobank', () => {
 
@@ -7,7 +8,7 @@ test.describe('Payment in Demobank', () => {
         await page.goto('/');
     });
 
-    test('Successful payment', async ({ page }) => {
+    test.only('Successful payment', async ({ page }) => {
         // Arrange
         const userId = loginData.userId;
         const userPassword = loginData.userPassword;
@@ -18,9 +19,10 @@ test.describe('Payment in Demobank', () => {
         const expectedMessage = `Przelew wykonany! ${transferAmount},00PLN dla ${transferReceiver}`;
 
         // Act
-        await page.getByTestId('login-input').fill(userId);
-        await page.getByTestId('password-input').fill(userPassword);
-        await page.getByTestId('login-button').click();
+        const loginPage = new LoginPage(page);
+        await loginPage.loginInput.fill(userId);
+        await loginPage.passwordInput.fill(userPassword);
+        await loginPage.loginButton.click();
 
         await page.getByRole('link', { name: 'płatności' }).click();
         await page.getByTestId('transfer_receiver').fill(transferReceiver);
